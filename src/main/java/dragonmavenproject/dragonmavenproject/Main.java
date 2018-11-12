5762208d7938f5fc2b31c2da25e8fbe0fe95ce4c
Hamzaoui Mohamed <mohamed.hamzaoui@dauphine.eu>
2018-11-13 00:47:22 +0100

    Add Main Test

---
+++ src/main/java/dragonmavenproject/dragonmavenproject/Main.java
@@ -0,0 +1,26 @@
+package Model;
+
+import java.security.spec.MGF1ParameterSpec;
+
+public class Main {
+	
+	 public Main()
+	   {   Magasin magasin = new Magasin(30);
+	       Produit prod =new Produit("EP", 30, "Arme");
+	       Produit prod1 =new Produit("EP", 30, "Arme");
+	       Dragon dragon = new Dragon("red",100,magasin);
+	        magasin.addProduit(prod);
+	        magasin.addProduit(prod);
+	        magasin.addObserver(dragon);
+
+	        magasin.addProduit(prod1);
+	        
+	        System.out.println(magasin.nbOccuranceProduit(prod));
+	        System.out.println(magasin.nbOccuranceProduit(prod1));
+	   }
+	   public static void main(String [] args)
+	   {
+	      Main m = new Main();
+	   }
+
+}
